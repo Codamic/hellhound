99dec72b2c12ff5da1fa780af44ea67edf0ca362
Sameer Rahmani <lxsameer@gnu.org>
2016-10-16 14:51:26 +0330

    codaina added and configred

--- project.clj
+++ project.clj
@@ -4,4 +4,19 @@
   :license {:name "GPLv3"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [cljsjs/jquery "2.2.4-0"]])
+                 [cljsjs/jquery "2.2.4-0"]]
+  :plugins [[funcool/codeina "0.4.0"
+             :exclusions [org.clojure/clojure]]]
+
+  :profiles {:dev {}
+             :clj {:codeina {:sources ["src"]
+                             :reader :clojure
+                               :target "doc/dist/latest/api"
+                             :src-uri "http://github.com/Codamic/hell-hound/blob/master/"
+                             :src-uri-prefix "#L" }}
+
+             :cljs {:codeina {:sources ["src"]
+                              :reader :clojurescript
+                              :target "doc/dist/latest/fontend/api"
+                              :src-uri "http://github.com/Codamic/hell-hound/blob/master/"
+                              :src-uri-prefix "#L"}}})
--- src/hell_hound/assets-pipeline/core.clj
+++
@@ -1,6 +0,0 @@
-(ns hell-hound.assets-pipeline.core)
-
-(defn middleware
-  "A middleware to manage the assets of the handler"
-  [handler]
-  (fn [request]) handler)
