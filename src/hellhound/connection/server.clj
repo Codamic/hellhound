cb8be20a96744e770e55264d27417654ca40ab11
Sameer Rahmani <lxsameer@gnu.org>
2016-11-20 15:42:22 +0330

    First prototype of server side system finished

--- src/hellhound/connection/server.clj
+++ src/hellhound/connection/server.clj
@@ -2,16 +2,19 @@
   (:require [hellhound.system :refer [get-system]]
             [compojure.core :as compojure :refer [GET POST]]))
 
+(defn- websocket
+  []
+  (:websocket (get-system)))
 
 (defn routes
   "Routes macro allows developer to setup sente connection and urls easy
   and painlessly."
   []
   (compojure/routes
-   (compojure/context "/hellhound" []
-                      (compojure/GET  "/" req ((:ring-ajax-get-or-ws-handshake (:websocket (get-system))) req))
-                      (compojure/POST "/" req ((:ring-ajax-post (:websocket (get-system)))) req))))
-
+   (compojure/context
+    "/hellhound" []
+    (compojure/GET  "/" req ((:ring-ajax-get-or-ws-handshake (websocket)) req))
+    (compojure/POST "/" req ((:ring-ajax-post (websocket))) req))))
 
 
 (defmulti router
@@ -24,13 +27,13 @@
   [{:as ev-msg :keys [event id ?data ring-req ?reply-fn send-fn]}]
   (let [session (:session ring-req)
         uid     (:uid     session)]
-    (println "Unhandled event: %s" event)
+    (println "Unhandled event: " event)
     (when ?reply-fn
       (?reply-fn {:umatched-event-as-echoed-from-from-server event}))))
 
-(defmethod router :sam/sam
-  [{:as ev-msg :keys [?data ?reply-fn event]}]
-  (spit "/home/lxsameer/tmpdata" event :append true))
+(defmethod router :chsk/ws-ping
+  [{:as ev-msg :keys [?data ?reply-fn event]}])
+
 
 (defn event-router [{:as ev-msg :keys [id ?data event]}]
   (router ev-msg))
