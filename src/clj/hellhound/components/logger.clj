3aeafaf681175f3aeb1be52266b6511869398318
Sameer Rahmani <lxsameer@gnu.org>
2017-05-01 22:07:52 +0100

    old boot namespace removed

--- src/clj/hellhound/components/logger.clj
+++ src/clj/hellhound/components/logger.clj
@@ -1,4 +1,6 @@
 (ns hellhound.components.logger
+  "Hellhound logger component is a core.async style logger which
+  uses a core.async thread for its business."
   (:require [hellhound.logger.core :refer :all]
             [hellhound.components.core :as component]
             [clojure.core.async        :as async]))
--- src/clj/hellhound/events.clj
+++
@@ -1,8 +0,0 @@
-(ns hellhound.events
-  "This namespace contains all the necessary means to handle client
-  side events.")
-
-(defmacro defrouter
-  "Create an event router to be used for dispatching client events"
-  [name body]
-  `(def ~name ~body))
