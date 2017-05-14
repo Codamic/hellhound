43abe67337c6091abc2f556466a5eae852d0117d
Sameer Rahmani <lxsameer@gnu.org>
2017-05-14 23:22:22 +0100

    db `migration` task complete to create new migration namespace

--- src/clj/hellhound/tasks/db.clj
+++ src/clj/hellhound/tasks/db.clj
@@ -1,10 +1,16 @@
 (ns hellhound.tasks.db
-  (:require [hellhound.tasks.core :refer [in-migrations]]
+  (:require [hellhound.tasks.core :refer [in-migrations epoch-time long-str info error warn]]
             [clojure.string       :refer [ends-with? starts-with?]]
-            [clj-time.core        :refer [before?]]))
+            [clojure.edn          :as edn]
+            [clj-time.core        :refer [before?]]
+            [clojure.java.io :as io]))
 
+;; DEFINATIONS
 
-(defn long-str [& strings] (clojure.string/join "\n" strings))
+;; Lock is not a great name i know, but i don't have
+;; time to think about it
+(def migrations-lock (atom []))
+(def migration-lock-file (io/resource "migration.lock"))
 (def migration_template
   (long-str "(ns db.migrations.%s)"
             ""
@@ -17,6 +23,26 @@
             "  []"
             "  ;; Your code to teardown this migration, goes here"
             ")"))
+
+;; FUNCTIONS
+(defn get-lock
+  "Get the migrations listed in the `migration.lock` file and reset the
+  global `migrations-lock` atom with the exist value. If there were
+  no migrations listed will ignore resetting the atom."
+  []
+  (if (nil? migration-lock-file)
+    (throw (Exception. "Can't find migration.lock file inside resource path."))
+    (let [migrations (edn/read-string (slurp migration-lock-file))]
+      (if-not (empty? migrations)
+        (reset! migrations-lock migrations)))))
+
+(defn update-lock
+  "Update the global `migrations-lock` atom with the given `nsname`
+  and flush the changes into `migration.lock` file."
+  [nsname]
+  (let [migrations (conj @migrations-lock nsname)]
+    (spit migration-lock-file (pr-str migrations))
+    (swap! migrations-lock (fn [_] migrations))))
 
 (defn- is-timestamp?
   [value]
@@ -39,40 +65,32 @@
          (filter (fn [x] (println (ns-name x)) (starts-with? (ns-name x) "db.migrations")))
          (sort #(before? (timestamp %1) (timestamp %2))))))
 
-
-(defn epoch-time
-  []
-  (quot (System/currentTimeMillis) 1000))
-
 (defn migrate
   [& rest]
-  (let [migrations (find-migrations)]
+  (let [migrations @migrations-lock]
     (if (empty? migrations)
-      (println "No migration found")
+      (warn "No migration found")
       (doseq [migration migrations]
         (println migration)))))
 
 (defn new-migrate
   [name & rest]
-  (println name)
   (let [epoch     (epoch-time)
         nsname    (str epoch "_" name)
         file-path (in-migrations (str nsname ".clj"))]
-    (println epoch)
-    (println nsname)
-    (println file-path)
+    (info "Creating migration file: " file-path)
     (spit file-path
-          (format migration_template nsname))))
+          (format migration_template nsname))
+    (info "Updating migrations lock file")
+    (update-lock nsname)))
 
 (defn wrong-command
   [cmd]
-  (println (format "Can't find the command '%s'." cmd)))
+  (error (format "Can't find the command '%s'." cmd)))
 
 (defn -main
   [command & rest]
-  (println command)
-  (println rest)
-  (println "<>")
+  (get-lock)
   (cond
     (= command "migration") (apply new-migrate rest)
     (= command "migrate")   (apply migrate rest)
