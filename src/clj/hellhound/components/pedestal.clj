cacca1e0dec9b3dbc39b7abe1d4eae078a3b58ba
Sameer Rahmani <lxsameer@gnu.org>
2017-06-12 21:56:50 +0100

    Basic pedestal component added

--- src/clj/hellhound/components/pedestal.clj
+++ src/clj/hellhound/components/pedestal.clj
@@ -4,9 +4,44 @@
   `Pedestal` component is responsible for creating a pedestal service
   by reading the configuration and service map from environment config
   file or from the given input."
-  (:require [hellhound.components.protocols :as protocols]))
+  (:require
+   [io.pedestal.http               :as http]
+   [hellhound.core                 :as hellhound]
+   [hellhound.components.protocols :as protocols]))
 
-(defrecord Pedestal []
+(declare map->Pedestal)
+
+;; Functions -----------------------------------------------
+(defn test?
+  []
+  (hellhound/test?))
+
+
+(defn new-pedestal
+  "Creates a new pedestal record."
+  [service-map service]
+  (map->Pedestal {:service-map service-map
+                  :serive      service}))
+
+(defn make-pedestal-component
+  "Create new pedestal record to with the key name of `:pedestal`
+  to be injected into `hellhound`'s system map"
+  [system-map service-map service]
+  (update-in system-map [:components :pedestal]
+             (new-pedestal service-map sercice)))
+
+;; Record --------------------------------------------------
+(defrecord Pedestal [service-map service]
   protocols/Lifecycle
-  (start [this])
-  (stop [this]))
+  (start [this]
+    (if service
+      this
+      (cond-> service-map
+        true                      http/create-server
+        (not (test? service-map)) http/start
+        true                      ((partial assoc this :service)))))
+
+  (stop [this]
+    (when (and service (not (test? service-map))))
+    (http/stop service)
+    (assoc this :service nil)))
