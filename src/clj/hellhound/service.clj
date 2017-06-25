66f95283be6ca87d030d1b4ca38117c0de7733e3
Sameer Rahmani <lxsameer@gnu.org>
2017-06-26 02:37:05 +0430

    service ns added to provide shortcuts for creating service maps

---
+++ src/clj/hellhound/service.clj
@@ -0,0 +1,33 @@
+(ns hellhound.service
+  "In order to create `service` maps to use with your hellhound
+  application, you have to use `create-service-map` function in
+  this namespace."
+  (:require
+   [io.pedestal.http :as http]
+   [hellhound.core  :as hellhound]))
+
+
+(def default-system-map
+  {:env                  (hellhound/env)
+   ::http/resource-path  (hellhound/fetch-config-key :public-files-path)
+   ::http/secure-headers {:content-security-policy-settings {:object-src "none"}}
+   ::http/type           :immutant
+   ::http/host           (hellhound/fetch-config-key :http-host)
+   ::http/port           (hellhound/fetch-config-key :http-port)
+   ;; TODO: Add immutant related features in the
+   ;; map blow
+   ::http/container-options {:h2c? true
+                             :h2? false}})
+
+
+(defn create-service-map
+  "Creates a system map and fills some default values for the map"
+  [system-map]
+  (merge default-system-map
+         system-map))
+
+(defmacro defservice-map
+  "A short cut for creating service maps using `create-service-map`
+  function."
+  [map-name service-map]
+  `(def ~map-name (hellhound.service/create-service-map ~service-map)))
