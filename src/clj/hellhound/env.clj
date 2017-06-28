e0b9b5f496f4d94a0494403d9025772239065cc3
Sameer Rahmani <lxsameer@gnu.org>
2017-06-28 22:59:28 +0100

    env ns added which contains runtime environment helpers

---
+++ src/clj/hellhound/env.clj
@@ -0,0 +1,25 @@
+(ns hellhound.env)
+
+;;;; Runtime Environment helpers ---------------------------
+(defn env
+  "Return the current runtime profile. Possible values
+  `development`, `test`, `production` or any custom
+  envrionment type which provided by `HH_ENV` environment
+  variable. the default value is `:development`."
+  []
+  (keyword (or (System/getenv "HH_ENV") "development")))
+
+(defn development?
+  "Returns `true` if current runtime profile is `:development`."
+  []
+  (= :development (env)))
+
+(defn test?
+  "Returns `true` if current runtime profile is `:test`."
+  []
+  (= :test (env)))
+
+(defn production?
+  "Returns `true` if current runtime profile is `:production`."
+  []
+  (= :production (env)))
