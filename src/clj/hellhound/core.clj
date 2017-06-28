52864058b9a49582bbde3ff95b91ec5fb3f553ef
Sameer Rahmani <lxsameer@gnu.org>
2017-06-28 22:59:46 +0100

    main core ns cleaned up to contains only shortcuts

--- src/clj/hellhound/core.clj
+++ src/clj/hellhound/core.clj
@@ -1,74 +1,7 @@
 (ns hellhound.core
-  "This namespace contains core functions which are required
-  by the whole framework in order to operate."
   (:require [hellhound.config :as config]
-            [clojure.java.io  :as io]))
+            [hellhound.env    :as env]))
 
-;; Definitions ---------------------------------------------
-(def environment-configuration (atom {}))
-
-;; Functions -----------------------------------------------
-;;;; Runtime Environment helpers ---------------------------
-(defn env
-  "Return the current runtime profile. Possible values
-  `development`, `test`, `production` or any custom
-  envrionment type which provided by `HH_ENV` environment
-  variable. the default value is `:development`."
-  []
-  (keyword (or (System/getenv "HH_ENV") "development")))
-
-(defn development?
-  "Returns `true` if current runtime profile is `:development`."
-  []
-  (= :development (env)))
-
-(defn test?
-  "Returns `true` if current runtime profile is `:test`."
-  []
-  (= :test (env)))
-
-(defn production?
-  "Returns `true` if current runtime profile is `:production`."
-  []
-  (= :production (env)))
-
-
-;;;; Runtime Environment Configuration Loaders -------------
-(defn- config-file
-  []
-  (format "environments/%s.edn" (name (env))))
-
-(defn load-runtime-configuration
-  "Read and parse the configuration file related to the current runtime
-  environment."
-  []
-  (let [config-data (config/read-config (config-file))]
-    (reset! environment-configuration config-data)
-    config-data))
-
-(defn application-config
-  "Return the current runtime environment configuration."
-  []
-  (let [config-data @environment-configuration]
-    (if (empty? config-data)
-      (load-runtime-configuration)
-      config-data)))
-
-(defn- default-value-for
-  [key-names]
-  (delay (get-in config/default-config)))
-
-(defn- derefiy
-  [value]
-  (if (delay? value)
-    value
-    (delay value)))
-
-(defn get-config
-  "Fetch the given key (or nested keys) from the environment config of
-  the project other. Returns the default value from hellhound.config"
-  [& keys]
-  (let [app-value (get-in (application-config) keys)]
-    (if (nil? app-value)
-      (default-value-for keys)
-      (derefiy app-value))))
+;; Shortcut functions --------------------------------------
+(def get-config config/get-config)
+(def env env/env)
