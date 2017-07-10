ca040857b7c3240cfeb7fbd8389af2cc0567c531
Sameer Rahmani <lxsameer@gnu.org>
2017-07-11 00:45:04 +0100

    remaning file added to the repo

---
+++ src/clj/hellhound/system/core.clj
@@ -0,0 +1,18 @@
+(ns hellhound.system.core
+  "All the functions for managing system state live in this namespace")
+
+
+;; Main storage for system data.
+(def system (atom {}))
+
+(defn get-system
+  []
+  @system)
+
+(defn update-system!
+  [fn]
+  (swap! system fn))
+
+(defn reset-system!
+  [value]
+  (reset! system value))
