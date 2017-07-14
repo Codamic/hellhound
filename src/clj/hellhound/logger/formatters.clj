8c6fad6934703f1207beb4202918bbb17329c2f9
Sameer Rahmani <lxsameer@gnu.org>
2017-07-15 00:02:28 +0100

    Really simple timbre base logger with almost no config added

---
+++ src/clj/hellhound/logger/formatters.clj
@@ -0,0 +1,26 @@
+(ns hellhound.logger.formatters
+  "Different types of log formatter functions to be used with
+  timbre for different environments."
+  {:author "Sameer Rahmani (@lxsameer)"}
+  (:require [taoensso.timbre :as timbre]))
+
+(def LEVELS {:trace (timbre/color-str :white "TRACE")
+             :debug (timbre/color-str :blue "DEBUG")
+             :info  (timbre/color-str :green   "INFO")
+             :warn  (timbre/color-str :yellow "WARN")
+             :error (timbre/color-str :red    "ERROR")
+             :fatal (timbre/color-str :purple    "FATAL")})
+
+(defn ^String process-level
+  [^clojure.lang.Keyword level-keyword]
+  (level-keyword LEVELS))
+
+
+(defn ^String default-dev-formatter
+  [^clojure.lang.PersistentArrayMap data]
+  (let [time   @(:timestamp_ data)
+        line   (:?line data)
+        level  (process-level (:level data))
+        ns-str (:?ns-str data)
+        msg    @(:msg_ data)]
+    (format "[%s] [%s] <%s:%s> - %s" time level ns-str line msg)))
