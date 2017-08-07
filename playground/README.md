9f4e99b5c71456752b4fd14928a92de1355d9ab8
Sameer Rahmani <lxsameer@gnu.org>
2017-08-07 22:27:55 +0100

    Playground added to classpath on development

---
+++ playground/README.md
@@ -0,0 +1,5 @@
+# Playground
+This directory contains playground namespaces which I use to develop hellhound.
+The playground code distributed in namespace based on time so I create a new
+namespace each month and keep all the development code in that month in that ns
+and move those actual actual source tree.
---
+++ playground/aug.clj
@@ -0,0 +1,136 @@
+(ns aug)
+
+(defprotocol ISystem
+  (start [this]
+    "Starts the system.")
+
+  (stop  [this]
+    "stops the system")
+
+  (restart [this]
+    "Restarts the system")
+
+  (components [this]
+    "Returns a hashmap of all the components defined in the system")
+
+  (get-component [this component-name]
+    "Return the instance of the component with the given name")
+
+  (update! [this key value]
+    "Updates the system with given value under the given key"))
+
+
+
+(defprotocol IComponent
+  "This protocol defines a very basic component for hellhound system."
+  (start! [component context]
+    "Starts the component.")
+
+  (stop!  [component]
+    "Stops the component.")
+
+  (started? [component]
+    "Returns a `true` if component started and `false` otherwise.")
+
+  (dependencies [component]
+    "Returns a vector of dependency names."))
+
+
+
+(extend-protocol IComponent
+  clojure.lang.PersistentArrayMap
+  (start! [this context]
+    (let [start-fn (::start-fn this)]
+      (start-fn this context)))
+
+  (stop! [this]
+    (let [stop-fn (::stop-fn this)]
+      (stop-fn this)))
+
+  (started? [this]
+    (or (::started? this) false))
+
+  (dependencies [this]
+    (::depends-on this)))
+
+(def example-component
+  {::depends-on [:sa]
+   ::name :server
+   ::started? true
+   ::start-fn (fn [this context] (println (str "<<<<" context)))
+   ::stop-fn (fn [this] (println "stoping"))})
+
+(def example-system
+  {:components [{}]})
+
+(extend-protocol ISystem
+  clojure.lang.PersistentArrayMap
+  (start! [this]
+    (:hellhound.component/start-fn this))
+
+  (components [this] (get-components this))
+
+  (get-component [this component-name]
+    (component-name (components this)))
+
+  (update! [this key value]
+    (swap! system #(update-in % key value))))
+
+
+
+
+(defn starting-context
+  "Generates the `context` map to be passed to `start!` of the component"
+  [component]
+  {})
+
+(defn call-start
+  "Calls `start!` function of the component and generates the context for it."
+  [component]
+  (when-not (started? component)
+    (start! component (starting-context component))))
+
+(defn component-dependencies
+  "Returns a vector of all the components of the `system` which the given
+  `component` depends on them."
+  [system component]
+  (map #(get-component system %
+                       (dependencies component))))
+
+(defn with-component-dependencies
+  "Runs the given function for all the dependencies of the given `component`
+  and the `component` itself and update the given `system` with the result
+  of the execution."
+  [system component f]
+  (let [components-map  (conj (component-dependencies system component)
+                              component)]
+    (updat! system
+                   :components
+                   (map f components-map))))
+
+(defn start-component!
+  "Starts the given `component` of the given `system`."
+  [system component]
+  (with-component-dependencies system component call-start))
+
+
+(defn stop-component!
+  "Stops the given `component` of the given `system`."
+  [system component]
+  (with-component-dependencies system component call-stop))
+
+(defn start-system!
+  "Starts the given `system` map."
+  [system]
+  (map #(start-component! system %) (components system)))
+
+
+(s/fdef get-components
+        :args (s/cat :system map?)
+        :ret vector?
+        :fn #(= (:ret %) (-> :args :system :components)))
+
+(defn get-components
+  "Returns the components catalog of the given `system`."
+  [system]
+  (:components system))
