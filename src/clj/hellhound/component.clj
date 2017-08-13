e69d3e6673d7bd5d1f71c5dc988151ba477397f5
Sameer Rahmani <lxsameer@gnu.org>
2017-08-13 01:08:26 +0100

    IComponent protocol and type extention added to component ns

--- src/clj/hellhound/component.clj
+++ src/clj/hellhound/component.clj
@@ -1,9 +1,27 @@
 (ns hellhound.component
-  (:require [hellhound.system.protocols :as protocols]))
+  (:require [hellhound.system.protocols :as protocols])
+  (:import (clojure.lang PersistentArrayMap)))
 
 
-(extend-protocol protocols/IComponent
-  clojure.lang.PersistentArrayMap
+(defprotocol IComponent
+  "This protocol defines a very basic component for hellhound system."
+  (start! [component context]
+    "Starts the component.")
+
+  (stop!  [component]
+    "Stops the component.")
+
+  (started? [component]
+    "Returns a `true` if component started and `false` otherwise.")
+
+  (get-name [component]
+    "Returns the name of the component.")
+
+  (dependencies [component]
+    "Returns a vector of dependency names."))
+
+(extend-type PersistentArrayMap
+  IComponent
   (start! [this context]
     (let [start-fn (::start-fn this)]
       (assoc (start-fn this context)
