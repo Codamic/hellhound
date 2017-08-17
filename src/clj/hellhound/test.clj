8603a6349a0679e9c7c8289cd9c54f2358098128
Sameer Rahmani <lxsameer@gnu.org>
2017-08-17 01:52:30 +0100

    ns-spec-tests function finished to check all the specs in the given ns

--- src/clj/hellhound/test.clj
+++ src/clj/hellhound/test.clj
@@ -1,47 +1,19 @@
 (ns hellhound.test
   (:require [clojure.test :as t]
+            [clojure.spec.test.alpha :as stest]
             [hellhound.system.core :as c]))
 
-;; (def a (:clojure.spec.test.check/ret (first (clojure.spec.test.alpha/check 'hellhound.system.core/get-components))))
-;; (keys a)
-;; (:result a)
-;; (:fail a)
-;; (ex-data (:clojure.test.check.properties/error (:result-data a)))
+(defn check
+  [x]
+  (let [check-results (stest/check x)
+        passed       (every? nil? (map :failure check-results))]
+    (t/is passed
+          ;;(first check-results)
+          x)))
 
-;; (defmacro testspec
-;;   [sym]
-;;   `(clojure.test/deftest test-name#
-;;      (clojure.test/testing (str "Testing spec for " (name ~sym))
-;;        (println "sadasdasd"))))
-
-
-
-;; (defmacro defspec-test
-;;   ([name sym-or-syms] `(defspec-test ~name ~sym-or-syms nil))
-;;   ([name sym-or-syms opts]
-;;    (when clojure.test/*load-tests*
-;;      `(def ~(vary-meta name assoc
-;;                        :test `(fn []
-;;                                 (let [check-results# (clojure.spec.test.alpha/check ~sym-or-syms ~opts)
-;;                                       checks-passed?# (every? nil? (map :failure check-results#))]
-;;                                   (println "---------------------------------")
-;;                                   (println checks-passed?#)
-;;                                   (if checks-passed?#
-;;                                     (clojure.test/do-report {:type    :pass}
-;;                                                   :message (str "Generative tests pass for "
-;;                                                                 (clojure.string/join ", " (map :sym check-results#))))
-;;                                     (doseq [failed-check# (filter :failure check-results#)
-;;                                             :let [r# (clojure.spec.test.alpha/abbrev-result failed-check#)
-;;                                                   failure# (:failure r#)]]
-;;                                       (println "------------")
-;;                                       (clojure.test/do-report
-;;                                        {:type     :fail
-;;                                         :message  (with-out-str (clojure.spec.alpha/explain-out failure#))
-;;                                         :expected (->> r# :spec rest (apply hash-map) :ret)
-;;                                         :actual   (if (instance? Throwable failure#)
-;;                                                     failure#
-;;                                                     (:clojure.spec.test.alpha/val failure#))})))
-;;                                   checks-passed?#)))
-;;         (fn [] (t/test-var (var ~name)))))))
-
-;; (macroexpand-1 (testspec 'hellhound.system.core/get-components))
+(defn ns-spec-tests
+  [ns-sym]
+  (doseq [[sym actual-var] (ns-publics ns-sym)
+          :let [full-symbol  (symbol (name ns-sym) (name sym))]]
+    (alter-meta! actual-var assoc :test #(check full-symbol))
+    (t/test-var actual-var)))
--- test/clj/hellhound/system/core_test.clj
+++ test/clj/hellhound/system/core_test.clj
@@ -1,68 +1,9 @@
 (ns hellhound.system.core_test
   (:require [hellhound.system.core :as sut]
-            [clojure.test :as t]
-            [clojure.string :as str]
-            [clojure.spec.alpha :as s]
+            [clojure.test :refer [deftest]]
             [hellhound.test :as ht]
             [clojure.spec.test.alpha :as stest]))
 
 
-;; (t/deftest get-components
-;;   (let [check-results (stest/check `hellhound.system.core/get-components)
-;;         passed       (every? nil? (map :failure check-results))]
-;;     (t/is passed (first check-results))))
-
-(defn check
-  [x]
-  (let [check-results (stest/check x)
-        passed       (every? nil? (map :failure check-results))]
-    (t/is passed
-          ;;(first check-results)
-          x)))
-
-;; (t/deftest all-the-spec
-;;   (doseq [sym (stest/enumerate-namespace 'hellhound.system.core)]
-;;     (t/testing (str "Checking specs of " sym)
-;;       (check sym))))
-
-
-;; (t/deftest all-the-spec
-;;   (doseq [sym (stest/enumerate-namespace 'hellhound.system.core)]
-;;     (t/testing (str "Checking specs of " sym)
-;;       (check sym))))
-
-
-;; (defn test-fn
-;;   [sym-or-syms opts]
-;;   `(fn []
-;;      (let [check-results# (clojure.spec.test.alpha/check ~sym-or-syms ~opts)
-;;            checks-passed?# (every? nil? (map :failure check-results#))]
-;;        (println "---------------------------------")
-;;        (println checks-passed?#)
-;;        (if checks-passed?#
-;;          (clojure.test/do-report {:type    :pass}
-;;                                  :message (str "Generative tests pass for "
-;;                                                (clojure.string/join ", " (map :sym check-results#))))
-;;          (doseq [failed-check# (filter :failure check-results#)
-;;                  :let [r# (clojure.spec.test.alpha/abbrev-result failed-check#)
-;;                        failure# (:failure r#)]]
-;;            (println "------------")
-;;            (clojure.test/do-report
-;;             {:type     :fail
-;;              :message  (with-out-str (clojure.spec.alpha/explain-out failure#))
-;;              :expected (->> r# :spec rest (apply hash-map) :ret)
-;;              :actual   (if (instance? Throwable failure#)
-;;                          failure#
-;;                          (:clojure.spec.test.alpha/val failure#))})))
-;;        checks-passed?#)))
-
-;; (doseq [sym (clojure.spec.test.alpha/enumerate-namespace 'hellhound.system.core)]
-;;   (println (meta (macroexpand (defspec-test sym)))))
-(defn spec-ns
-  [ns-sym]
-  (doseq [[sym actual-var] (ns-publics ns-sym)
-          :let [full-symbol  (symbol (name ns-sym) (name sym))]]
-    (alter-meta! actual-var assoc :test #(check full-symbol))
-    (t/test-var actual-var)))
-
-(spec-ns 'hellhound.system.core)
+(deftest spec-test
+  (ht/ns-spec-tests 'hellhound.system.core))
