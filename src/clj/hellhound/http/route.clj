8badb49811a73d6a2c4a244ecc15c35e54e90741
Sameer Rahmani <lxsameer@gnu.org>
2017-08-21 00:05:03 +0100

    Basic websocket endpoint added to the router

--- src/clj/hellhound/http/route.clj
+++ src/clj/hellhound/http/route.clj
@@ -3,8 +3,10 @@
   DOCTODO"
   (:require
    [clojure.spec.alpha :as s]
-;;   [bidi.bidi          :as bidi]
    [bidi.ring          :as bring]
+   [manifold.stream    :as stream]
+   [aleph.http         :as http]
+   [hellhound.logger   :as log]
    [hellhound.core     :as hellhound]))
 
 (defn hello
@@ -13,9 +15,20 @@
    :headers []
    :body "Yeah"})
 
+(defn ws
+  [req]
+  (log/info "Accpting WS connection")
+  (let [input-stream @(http/websocket-connection req)]
+    (stream/connect input-stream input-stream)))
+
+
 (def routes
   (bring/make-handler
-   ["/" {:get {"" hello}}]))
+   ["/"
+    {:get {"/" hello
+           "ws/" ws}}]))
+
+
 
 ;; (bidi/match-route hellhound-routes "/")
 ;; (bidi/path-for hellhound-routes :something)
