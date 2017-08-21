990aac07157493a766213fd64920ff8fde52b1a9
Sameer Rahmani <lxsameer@gnu.org>
2017-08-21 10:36:56 +0100

    PoC event router implemented using manifold streams

--- src/clj/hellhound/http/route.clj
+++ src/clj/hellhound/http/route.clj
@@ -5,6 +5,7 @@
    [clojure.spec.alpha :as s]
    [bidi.ring          :as bring]
    [manifold.stream    :as stream]
+   [manifold.deferred  :as deferred]
    [aleph.http         :as http]
    [hellhound.logger   :as log]
    [hellhound.core     :as hellhound]))
@@ -15,12 +16,34 @@
    :headers []
    :body "Yeah"})
 
+(def event-router
+  {:hello #(stream/put! (:stream %) "hey!")})
+
+(defn resolve-route
+  [input-stream router msg]
+  (let [handler (:hello router)]
+    (handler {:stream input-stream :msg msg})))
+
+(defn setup-event-router
+  [router-stream router]
+  (stream/consume #(resolve-route router-stream event-router %) router-stream))
+
+(defn create-ws
+  [req]
+  (-> (http/websocket-connection req)
+      (deferred/catch Exception #(throw %))))
+
 (defn ws
   [req]
   (log/info "Accpting WS connection")
-  (let [input-stream @(http/websocket-connection req)
-        router-stream (stream/stream 100)]
-    (stream/connect input-stream input-stream)))
+  (let [input-stream  (create-ws req)
+        router-stream (stream/stream 100)
+        output-stream (stream/stream 100)]
+
+    (stream/consume #(println %) output-stream)
+    (setup-event-router router-stream)
+    (stream/connect input-stream router-stream)
+    (stream/connect router-stream output-stream)))
 
 
 (def routes
