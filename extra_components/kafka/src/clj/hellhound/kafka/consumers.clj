a1e2d09d1f62e612374b52ff418a4963e3b07fbd
Sameer Rahmani <lxsameer@gnu.org>
2018-03-31 22:00:41 +0100

    type hints and docstrings have been added to hellhound.kafka.consumer ns

--- extra_components/kafka/src/clj/hellhound/kafka/consumers.clj
+++ extra_components/kafka/src/clj/hellhound/kafka/consumers.clj
@@ -2,14 +2,21 @@
   (:require
    [hellhound.kafka.core :as core])
   (:import
-   [org.apache.kafka.clients.consumer KafkaConsumer]
+   [org.apache.kafka.clients.consumer
+    KafkaConsumer
+    Consumer]
    [org.apache.kafka.common.serialization StringDeserializer]))
 
 
 (def default-key-serializer (StringDeserializer.))
 (def default-value-deserializer (StringDeserializer.))
 
-(defn make-consumer
+(defn ^KafkaConsumer make-consumer
+  "Create a kafka consumer using the given `config` and key value serializers.
+
+  `key-deserializer` and `value-deserializer` are going to deserialize the
+  respected value from kafka to clojure values. The default deserializer
+  for both of them is `StringDeserializer`."
   ([config]
    (make-consumer config
                   default-key-serializer
@@ -20,21 +27,28 @@
      (KafkaConsumer. props key-deserializer value-deserializer))))
 
 (defn subscribe
-  [consumer topics]
+  "Subscribe to the given list of `topics` of the given `consumer`."
+  [^Consumer consumer topics]
   (.subscribe consumer topics))
 
 (defn consume
-  ([consumer f]
+  "Consume records from the given `consumer` and apply function`f` on them.
+
+  By default consumed values are a `ConsumerRecords` java object.
+  An optional third arg is the `timeout` for the connection. The default value
+  for the timeout is `2000ms`."
+  ([^Consumer consumer f]
    ;; TODO: fetch the default timeout value from global config
    (consume consumer f 2000))
 
-  ([consumer f timeout]
+  ([^Consumer consumer f timeout]
    (try
      (while true
        (let [records (.poll consumer timeout)]
          (f records)))
      (finally
        (.close consumer)))))
+
 
 (comment
   (def c (make-consumer {"bootstrap.servers" "localhost:9092"
