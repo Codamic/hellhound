445327e6941bd31d004989dd97fe1f82dbb4d0d1
Sameer Rahmani <lxsameer@gnu.org>
2018-04-02 02:06:33 +0100

    The first working version of kafka producers has been added

--- extra_components/kafka/src/clj/hellhound/kafka/producers.clj
+++ extra_components/kafka/src/clj/hellhound/kafka/producers.clj
@@ -11,24 +11,41 @@
    [org.apache.kafka.common.serialization
     StringSerializer]))
 
-(def default-key-serializer StringSerializer)
-(def default-value-serializer StringSerializer)
+(def default-key-serializer (StringSerializer.))
+(def default-value-serializer (StringSerializer.))
 
 (defn ^KafkaProducer make-producer
   ([config]
-   (make-producer config default-key-serializer default-value-deserializer))
+   (make-producer config default-key-serializer default-value-serializer))
 
   ([config key-serializer value-serializer]
    (let [producer-config (core/make-config config)]
-     (KafkaProducer. config key-serializer value-serializer))))
+     (KafkaProducer. producer-config key-serializer value-serializer))))
 ;
 
 (defn send
-  [^Producer producer topic k v]
-  (let [record (ProducerRecord. topic k v)]
-    (.send producer record)))
+  ([^Producer producer topic v]
+   (let [record (ProducerRecord. topic v)]
+     (.send producer record)))
 
+  ([^Producer producer topic k v]
+   (let [record (ProducerRecord. topic k v)]
+     (.send producer record)))
+
+  ([^Producer producer topic partition k v]
+   (let [record (ProducerRecord. partition topic k v)]
+     (.send producer record)))
+
+  ([^Producer producer topic partition k v header]
+   (let [record (ProducerRecord. partition topic k v header)]
+     (.send producer record))))
+
+(defn close
+  [^Producer producer]
+  (.close producer))
 
 (comment
-  (let [p (make-producer {"bootstrap.servers" "localhost:9092"})]
-    (send p "something")))
+  (def p (make-producer {"bootstrap.servers" "localhost:9092"}))
+  (send p "something" "11111111")
+  (send p "something" "22222")
+  (close p))
