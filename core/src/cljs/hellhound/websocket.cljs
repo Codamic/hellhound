655c09926896ff75de885742237bd61753bd4d98
Sameer Rahmani <lxsameer@gnu.org>
2018-05-14 21:55:10 +0100

    old cljs files have been removed

--- core/src/cljs/hellhound/websocket.cljs
+++
@@ -1,5 +0,0 @@
-(ns hellhound.websocket
-  "In order to connect to the HellHound's websocket server
-  you need to use functions provided by this ns."
-  (:require
-   [hellhound.logger       :as log]))
--- core/src/cljs/hellhound/websocket/events.cljs
+++
@@ -1,7 +0,0 @@
-(ns hellhound.websocket.events
-  (:require [re-frame.core :as re-frame]))
-
-
-(re-frame/reg-fx
- :websocket
- (fn [{:keys [:connect :send :disconnect]}]))
