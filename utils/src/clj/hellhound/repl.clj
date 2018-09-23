a765c9e38ec1db3c6bc6af2e192e917c23bc7a90
Sameer Rahmani <lxsameer@gnu.org>
2018-09-23 22:34:31 +0100

    REPL component has been removed in favor of the system wide repl implementation

---
+++ utils/src/clj/hellhound/repl.clj
@@ -0,0 +1,26 @@
+(ns hellhound.repl
+  (:require
+   [nrepl.server :as nrepl-server]
+   [hellhound.system :as sys]
+   [hellhound.logger :as logger]))
+
+(defn nrepl-handler []
+  (require 'cider.nrepl)
+  (ns-resolve 'cider.nrepl 'cider-nrepl-handler))
+
+
+(defn start!
+  []
+  (logger/info "Running nREPL on localhost:4000...")
+  (sys/set-system!
+   (assoc (sys/system)
+          ::server
+          (nrepl-server/start-server :port 4000 :handler (nrepl-handler)))))
+
+
+(defn stop!
+  []
+  (when-let [server (::server (sys/system))]
+    (logger/info "Stopping nREPL...")
+    (nrepl-server/stop-server server))
+  (sys/set-system! (dissoc (sys/system) ::server)))
